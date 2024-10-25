fa1ae9bddacffe39ef33727891588ce4f50f5c36
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-10-25 16:16:05 +0300

    Added queue function definition

--- queue.h
+++ queue.h
@@ -5,3 +5,11 @@
 typedef struct Queue Queue;
 
 bool queueCreate(Queue **queue);
+
+bool enqueue(Queue *queue, int value);
+
+bool dequeue(Queue *queue, int *value);
+
+bool isEmpty(Queue *queue);
+
+void queueDispose(Queue *queue);
