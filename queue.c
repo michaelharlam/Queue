cd1f4a536066e94634c76ba0b767ce0efd2136a5
Dmitrii Bordukov <zst@localhost>
2024-10-25 16:26:45 +0300

    isEmpty()

--- queue.c
+++ queue.c
@@ -1,11 +1,20 @@
 #include "queue.h"
 
+#include <stdlib.h>
+
 typedef struct Node {
-	int value;
-	struct Node *next;
+    int value;
+    struct Node *next;
 } Node;
 
 typedef struct Queue {
-	Node *front;
-	Node *back;
+    Node *front;
+    Node *back;
 } Queue;
+
+bool isEmpty(Queue *queue) {
+    if (queue->front == NULL) {
+        return false;
+    }
+    return true;
+}
