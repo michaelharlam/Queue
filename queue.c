1642b8bc6f5355f7d6c6894945d9cc437b499535
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-10-25 16:32:40 +0300

    Changed tabs -> spaces and added enqueue

--- queue.c
+++ queue.c
@@ -1,11 +1,28 @@
 #include "queue.h"
 
+#include <stdlib.h>
+
 typedef struct Node {
-	int value;
-	struct Node *next;
+    int value;
+    struct Node *next;
 } Node;
 
 typedef struct Queue {
-	Node *front;
-	Node *back;
+    Node *front;
+    Node *back;
 } Queue;
+
+bool enqueue(Queue *queue, int value) {
+    Node *node = malloc(sizeof(Queue));
+    if (node == NULL) {
+        return false;
+    }
+
+    if (!isEmpty(queue)) {
+        queue->front->next = node;
+    } else {
+        queue->back = node;
+    }
+    queue->front = node;
+    return true;
+}
